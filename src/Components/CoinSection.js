a896167d93111fd1b67e04531dc8c970b73c1337
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-09-14 11:03:13 +0600

    last commit

--- src/Components/CoinSection.js
+++ src/Components/CoinSection.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import ShowCard from './ShowCard';
-import SingeleCoinDetails from './SingeleCoinDetails';
+// import SingeleCoinDetails from './SingeleCoinDetails';
 
 
 const CoinSection = () => {
 
-    const seeDetails = (id) => {
+    // const seeDetails = (id) => {
 
-        <SingeleCoinDetails id={id} />
-    }
+    //     <SingeleCoinDetails id={id} />
+    // }
     const [coins, setcoins] = useState([]);
     useEffect(() => {
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false').then(res => res.json()).then(data => setcoins(data))
@@ -17,11 +17,11 @@ const CoinSection = () => {
     return (
         <div>
             <h1>This is coin section</h1>
-            
+
             <div className='row'>
                 {
                     coins.map(coin => <ShowCard
-                        seeDetails={seeDetails}
+
                         coin={coin} key={coin.id}></ShowCard>)
                 }
             </div>
--- src/Components/ShowCard.js
+++ src/Components/ShowCard.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import '../Components/Showcard.css'
+import { Link } from 'react-router-dom';
 
-const ShowCard = ({ coin, seeDetails }) => {
+const ShowCard = ({ coin }) => {
     const { id, symbol, name, image } = coin;
     return (
         <div className='col-lg-4 gx-2 gy-2'>
-            <Card className='shadow'>
-                <Card.Img className='coins-image mx-auto mt-4' src={image} />
-                <Card.Body>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>
-                        <p><span className='fw-bold'>Symbol:</span>${symbol}</p>
-                        <button className='btn btn-danger' > <link rel="stylesheet" href=""/> Details</button>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <Link>
+                <Card className='shadow'>
+                    <Card.Img className='coins-image mx-auto mt-4' src={image} />
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>
+                            <p><span className='fw-bold'>Symbol:</span>${symbol}</p>
+                            {/* <button className='btn btn-danger' > <link rel="stylesheet" href=""/> Details</button> */}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Link>
         </div>
     );
 };
