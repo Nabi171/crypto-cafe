7df7e19f96907e199b6702f837de44c9e70ba828
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-09-16 23:39:36 +0600

    updare

--- src/Components/SingeleCoinDetails.js
+++ src/Components/SingeleCoinDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SingeleCoinDetails = () => {
     return (
         <div>
-            <h1>this is coin details page ..</h1>
+            <h1>this is coin details page ...</h1>
         </div>
     );
 };
