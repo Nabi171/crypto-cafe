f2b1d6b13147135d8f8ef1afab7300725ba5dffc
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-09-17 13:20:35 +0600

    end comi

--- src/Components/ShowCard.js
+++ src/Components/ShowCard.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import '../Components/Showcard.css'
 import { Link } from 'react-router-dom';
+import { fadeIn } from 'react-animations';
+import { bounce } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
 
 const ShowCard = ({ coin }) => {
+    const styles = {
+        bounce: {
+            animation: 'x 1s',
+            animationName: Radium.keyframes(bounce, 'bounce')
+        }
+    }
     const { id, symbol, name, image } = coin;
     return (
-        <div className='col-lg-4 gx-2 gy-2'>
+        <div style={styles.bounce} className='col-lg-4 gx-2 gy-2 test'>
             <Link to={`/coindetails/${id}`}>
                 <Card className='shadow'>
                     <Card.Img className='coins-image mx-auto mt-4' src={image} />
@@ -14,6 +23,7 @@ const ShowCard = ({ coin }) => {
                         <Card.Title>{name}</Card.Title>
                         <Card.Text>
                             <p><span className='fw-bold'>Symbol:</span>${symbol}</p>
+                            
                             {/* <button className='btn btn-danger' > <link rel="stylesheet" href=""/> Details</button> */}
                         </Card.Text>
                     </Card.Body>
